2a0fb014fa7c93417ba3f77b643d37aa8b3dacce
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-02-09 11:27:15 -0500

    Render list of all friends

--- friendlist/src/App.js
+++ friendlist/src/App.js
@@ -4,13 +4,14 @@ import { Header } from './components/Header';
 import { FriendList } from './components/FriendList';
 import { Footer } from './components/Footer';
 
+import { friendData } from './data/friends';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <FriendList />
+        <FriendList friendData={friendData} />
         <Footer />
       </div>
     );
--- friendlist/src/components/FriendList.js
+++ friendlist/src/components/FriendList.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { friendData } from '../data/friends';
 
-export class FriendList extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      friendData
-    };
-  }
-  render(){
-    console.log(this.state.friendData);
-    return (
-      <div className="friend-list">
-        <h2>My Friends</h2>
-        <div>All My Friends</div>
-        <div>My Best Friends</div>
-        <div>Average Age of My Best Friends</div>
+export const FriendList = ({friendData}) => {
+  return (
+    <div className="friend-list">
+      <h2>My Friends</h2>
+      <div>
+        <h2>All My Friends</h2>
+        <ul>
+          {
+            friendData.map(({first, last}) => (
+              <li key={last}>{first} {last}</li>
+            ))
+          }
+        </ul>
       </div>
-    );
-  }
-}
+      <div>My Best Friends</div>
+      <div>Average Age of My Best Friends</div>
+    </div>
+  );
+};
