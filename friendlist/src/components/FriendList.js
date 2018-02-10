f223787a9a24300bc329fa3a24a875b80ae286da
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-02-10 14:12:26 -0500

    Implement FriendList component - Add method to render a list of names - Add method to calculate average age of friends

--- friendlist/src/components/FriendList.js
+++ friendlist/src/components/FriendList.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const FriendList = ({friendData}) => {
-  return (
-    <div className="friend-list">
-      <h2>My Friends</h2>
-      <div>
-        <h2>All My Friends</h2>
-        <ul>
-          {
-            friendData.map(({first, last}) => (
-              <li key={last}>{first} {last}</li>
-            ))
-          }
-        </ul>
+export class FriendList extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  renderNames(list){
+    return list.map(({first, last}) => (<li>{first} {last}</li>));
+  }
+
+  avergeAge(list){
+    const sumOfAge = list.reduce((acc, friend) => acc += friend.age, 0);
+    return Math.round(sumOfAge / list.length);
+  }
+
+  render(){
+    const allFriends = this.props.friendData;
+    const bestFriends = allFriends.filter(friend => friend.isBestFriend);
+    const avgAge = this.avergeAge(bestFriends);
+
+    return (
+      <div className="friend-list">
+        <h2>Facts About My Friends</h2>
+        <div>
+          <h2>All My Friends</h2>
+          <ul>{ this.renderNames(allFriends) }</ul>
+        </div>
+        <div>
+          <h2>All My Best Friends</h2>
+          <ul>{ this.renderNames(bestFriends) }</ul>
+        </div>
+        <div><h2>The average age of my best friends is { avgAge }</h2></div>
       </div>
-      <div>My Best Friends</div>
-      <div>Average Age of My Best Friends</div>
-    </div>
-  );
-};
+    );
+  }
+}
