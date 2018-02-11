afce521d45046650ba2c358e5915bbcd81346047
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-02-11 17:48:27 -0500

    Add AddButton

---
+++ friendlist/src/components/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Button = () => {
+  return (
+    <input type="button" value="Add a new list" />
+  );
+}
--- friendlist/src/components/Header.js
+++ friendlist/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from './Button';
 
 export const Header = () => {
   return (
     <header className="Header">
       <h1>FriendList</h1>
+      <Button />
     </header>
   );
 }
